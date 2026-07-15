4be310ddbd53de20d0534ea30313d11483c4b76e
George Kontsevich <kxygk@aliyun.com>
2026-07-15 15:00:17 +0800

    Update: more Pathom tests

--- pathom-prectest.clj
+++ pathom-prectest.clj
@@ -75,3 +75,183 @@
                 :users/email])
 ;; {:customers/phone_number 6666, :fat-key 66, :users/email "66@66.com"}
 
+
+;; !!!!!!!!!!!!!!!!!!!!!!!!
+;; MORE TESTS
+;; !!!!!!!!!!!!!!!!!!!!!!!!
+
+;; ;;;;;;
+;; Fat Pack test
+;;
+;; This forces a resolver to run
+
+(pco/defresolver get-user-with-customer
+  [{:users/keys [id]}]
+  {::pco/output [{:fat-pack [:users/id
+                             :users/email
+                             :users/customer_id
+                             :customers/id
+                             :customers/billing_number
+                             :customers/phone_number]}]}
+  (println "get-user-with-customer FAT triggered")
+  {:fat-pack {:users/id                 66
+              :users/email              "66@66.com"
+              :users/customer_id        id
+              :customers/id             id
+              :customers/billing_number 666
+              :customers/phone_number   6666}})
+
+(def env
+  (pci/register [get-user
+                 get-customer
+                 get-user-with-customer
+                 user-customer-bridge]))
+
+(p.eql/process env
+               {:users/id 1000}
+               [{:fat-pack [:customers/phone_number
+                            :users/email]}])
+;;{:fat-pack {:customers/phone_number 6666, :users/email "66@66.com"}}
+;;
+;; This works but the result is in an extra layer of wrapping
+;; (next part resolves this)
+;;
+
+;; ;;;;;;
+;; Nested Inputs
+;;
+;; This allows the `:fat-pack` to be transparently consumed
+;;
+
+(pco/defresolver fat-eater
+  [{:keys [fat-pack]}]
+  {::pco/input[{:fat-pack [:users/id
+                           :users/email
+                           :users/customer_id
+                           :customers/id
+                           :customers/billing_number
+                           :customers/phone_number]}]
+   ::pco/output [:response]}
+  (println "fat-eater triggered")
+  {:response (str "yum, just ate: "
+                  (:users/id fat-pack))})
+
+
+(def env
+  (pci/register [get-user
+                 get-customer
+                 get-user-with-customer
+                 user-customer-bridge
+                 fat-eater]))
+
+(p.eql/process env
+               {:users/id 1000}
+               [:response])
+;; {:response "yum, just ate: 66"}
+
+
+;; BRANCHING PIPELINES
+;; How to force a pipeline to take an injexted custom detour
+
+
+;; ;;;;;;
+;; Pipeline Example
+
+(pco/defresolver A
+  [{::keys [start]}]
+  {::a-2-b (+ start
+             1)})
+
+(pco/defresolver B
+  [{::keys [a-2-b]}]
+  {::b-2-c (+ a-2-b
+             998)})
+
+(pco/defresolver C
+  [{::keys [b-2-c]}]
+  {::result b-2-c})
+
+(def env2
+  (pci/register [A
+                 B
+                 C]))
+
+(p.eql/process env2
+               {::start 0.0}
+               [::result])
+;;#:user{:result 999.0}
+
+;; ;;;;;;
+;; Branch selection key on input
+
+(pco/defresolver XXX
+  [{::keys [a-2-b
+            xxx-key]}]
+  {::b-2-c (+ a-2-b
+              665)})
+
+(def env2
+  (pci/register [A
+                 B
+                 C
+                 XXX]))
+
+(p.eql/process env2
+               {::start 0.0}
+               [::result])
+;;#:user{:result 999.0}
+
+(p.eql/process env2
+               {::start 0.0
+                ::xxx-key nil}
+               [::result])
+;; #:user{:result 666.0}
+
+;; ;;;;;;
+;; Branch selection key on output - DOESN'T WORK
+
+(pco/defresolver YYY
+  [{::keys [a-2-b]}]
+  {::b-2-c (+ a-2-b
+              968)
+   ::yyy-key nil})
+
+(def env3
+  (pci/register [A
+                 B
+                 C
+                 XXX
+                 YYY]))
+
+(p.eql/process env3
+               {::start 0.0}
+               [::result])
+;;#:user{:result 969.0}
+
+(p.eql/process env3
+               {::start 0.0}
+               [::result
+                ::yyy-key])
+;;#:user{:result 969.0, :yyy-key nil}
+
+;; Registering resolvers in a different order doesn't change anything
+;; YYYY is always preferred.
+;; Maybe because it has the same input requirements but provides two outputs instead of one
+
+(def env4
+  (pci/register [YYY
+                 A
+                 B
+                 C
+                 XXX]))
+
+
+(p.eql/process env4
+               {::start 0.0}
+               [::result])
+;;#:user{:result 969.0}
+
+(p.eql/process env4
+               {::start 0.0}
+               [::result
+                ::yyy-key])
